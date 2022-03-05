707593f01546195ddb46e61deae00031c09037ff
Jim Driscoll <jim.a.driscoll@gmail.com>
2022-03-05 12:10:01 +0000

    Fix the invalidate test

--- test/Invalidate.test.ts
+++ test/Invalidate.test.ts
@@ -8,6 +8,8 @@ describe("Delayed (Invalidate)", () => {
         const f = new Invalidate.DelayedStatefulPromise(async () => i++)
         assert(f.value === undefined)
         await new Promise(resolve => setTimeout(resolve, 0))
+        assert.equal(f.value, undefined)
+        await new Promise(resolve => setTimeout(resolve, 0))
         assert(f.value === 0)
         f.value = undefined
         assert(f.value === undefined)
@@ -17,12 +19,14 @@ describe("Delayed (Invalidate)", () => {
     it("Can be invalidated", async () => {
         let i = 0
         const f = new Invalidate.DelayedStatefulPromise(async () => i++)
-        assert(f.value === undefined)
+        assert.equal(f.value, undefined)
         await new Promise(resolve => setTimeout(resolve, 0))
-        assert(f.value === 0)
+        assert.equal(f.value, undefined)
+        await new Promise(resolve => setTimeout(resolve, 0))
+        assert.equal(f.value, 0)
         f.invalidate()
-        assert(f.value === 0)
+        assert.equal(f.value, 0)
         await new Promise(resolve => setTimeout(resolve, 0))
-        assert(f.value === 1)
+        assert.equal(f.value, 1)
     })
 })
